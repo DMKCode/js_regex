0ce1677e0b34051db91973d0d7e6cfcbe3fd1c1f
David Kananda <davidkananda@gmail.com>
2017-08-07 07:03:57 +0100

    playground setup

---
+++ app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const output = (str, regex, target) => {
+    target.innerHTML = str.replace(regex, str => `<span>${str}</span>`);
+}
+
+var str = `Is this This?`;
+
+//var regex = new RegExp("is", "g");
+var regex = /is/g;
+
+output(str, regex, document.querySelector('pre'));
+
+// console.log(regex.test(str));
+// console.log(regex.exec(str));
+// console.log(regex.exec(str));
+// console.log(regex.exec(str));
+// console.log(regex.exec(str));
+
+// console.log(str.match(regex));
+
+// console.log(str.replace(regex, str => "XX"));
+
+// console.log(str.search(regex));
