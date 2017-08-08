2c6b0579b19545cf7cd50475ad39214be5dec925
David Kananda <davidkananda@gmail.com>
2017-08-08 06:51:55 +0100

    sets of characters

---
+++ app-3.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+var str = `cat mat bat Hat ?at 0at`;
+// var regex = /[bc]at/g;
+// var regex = /[cb]at/g;
+// var regex = /[^cb]at/g;
+// var regex = /[a-z]at/g;
+// var regex = /[a-zA-Z]at/g;
+// var regex = /[a-dA-Z]at/g;
+// var regex = /[^a-zA-Z]at/g;
+// var regex = /[a-zA-Z0-9]at/g;
+var regex = /[a-zA-Z0-9?]at/g;
+
+/**
+ * @param String str
+ * @param RegExp regex
+ * @param HTMLElement target
+ */
+const output = (str, regex, target) => {
+    target.innerHTML = str.replace(regex, str => `<span>${str}</span>`);
+}
+
+output(str, regex, document.querySelector('pre'));
+
